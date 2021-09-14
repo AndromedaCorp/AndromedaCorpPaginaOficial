d9861091bd258e871e2bb8d4019e534a9b9ccc89
Hector Bonilla <andromedaCorp2.0@outlook.com>
2021-09-13 18:52:11 -0600

    Inicializacion de archivos en Framework Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LibrosComponent } from 'src/app/libros/libros.component';
+import { IndexComponent } from 'src/app/index/index.component';
+
+const routes: Routes = [
+  {
+    path:'Libros', component:LibrosComponent
+  },
+  {
+    path:'Index', component:IndexComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibrosComponent } from './libros/libros.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibrosComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
